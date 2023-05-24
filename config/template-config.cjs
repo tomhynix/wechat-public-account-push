786b1a7fcbb7c80ba7756f144353c096b8b85b67
tomhynix <69372296+tomhynix@users.noreply.github.com>
2023-05-24 22:21:18 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -2,29 +2,29 @@
 const TEMPLATE_CONFIG = [
   {
     id: '0001',
-    title: '亲爱的, 早上好',
+    title: '宝贝, 早上好',
     desc: `
       **{{date.DATA}}**
       
-      下个休息日：{{holidaytts.DATA}}
+      📅下个休息日：{{holidaytts.DATA}}
       
       ---
       
-      城市：{{city.DATA}}
+      🏠城市：{{city.DATA}}
       
-      天气：{{weather.DATA}}
+      🌈天气：{{weather.DATA}}
       
       气温(最高/最低):{{max_temperature.DATA}} / {{min_temperature.DATA}}
       
-      风向: {{wind_direction.DATA}}
+      →风向: {{wind_direction.DATA}}
       
-      风级: {{wind_scale.DATA}}
+      🌀风级: {{wind_scale.DATA}}
       
       {{comprehensive_horoscope.DATA}}
       
       ---
       
-      今天是我们相识的第{{love_day.DATA}}天
+      今天是我们在一起的第{{love_day.DATA}}天
       
       {{birthday_message.DATA}}
       
@@ -32,7 +32,8 @@ const TEMPLATE_CONFIG = [
       
       {{moment_copyrighting.DATA}}
       
-      {{poetry_title.DATA}} {{poetry_content.DATA}}
+      {{poetry_title.DATA}} 
+      {{poetry_content.DATA}}
     `
   },
   {
