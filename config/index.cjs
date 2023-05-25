bb981af1ad29f236e878e67fa79dd192a4e4af22
tomhynix <69372296+tomhynix@users.noreply.github.com>
2023-05-25 22:53:42 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -26,7 +26,7 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '2001', date: '11-15',
+          type: '生日', name: '恰娜', year: '2001', date: '11-15',
         },
         {
           type: '节日', name: '相识纪念日', year: '2023', date: '04-10',
